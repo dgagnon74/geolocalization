7cd018a6e19b46e41763429d51d7eaac595698ff
david <dgagnon74@gmail.com>
2019-10-02 11:45:22 -0400

    Add Business Logic

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# geolocalization
+# Geolocalization Service
+This Micro-Service is responsible to handle requests related to longitude & latitude. 
+Initial requirements are to provide the timezone related to a position defined in longitude & latitude.
+
+# Interface Documentation
+Interface is described using Swagger. Note that the Swagger file is used to generate the data model and has an input 
+for the corresponding API Gateway (enabling Authentication & QoS support).
+
+# Build
+mvnw clean install
+
+# Run
+mvnw clean install
+
+
+# Use
+For example, for Montreal, Quebec, Canada latitude is 45.508888, and the longitude is -73.561668
+So using CURL
+curl --request GET "http://127.0.0.1:9091/api/localization/v1/timezones?latitude=45.508888&longitude=-73.561668"
+
+JSON returned:
+{"longitude":-73.561668,"latitude":45.508888,"timezoneId":"America/Toronto"}
--- src/main/java/com/airgraft/services/geolocalization/controllers/LocalizationController.java
+++ src/main/java/com/airgraft/services/geolocalization/controllers/LocalizationController.java
@@ -7,21 +7,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import net.iakovlev.timeshape.TimeZoneEngine;
+
+import java.util.Optional;
+import java.time.ZoneId;
+
+
+import javax.annotation.PostConstruct;
 
 
 @RestController
-@RequestMapping(value = "v1/localization")
+@RequestMapping(value = "api/localization/v1")
 public class LocalizationController {
 
     static Logger LOG = LoggerFactory.getLogger(LocalizationController.class);
 
-    @RequestMapping(value = "/timezones", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public static ResponseEntity<GeolocInfo> getLocation(@RequestParam(name = "longitude") Double longitude, @RequestParam(name = "latitude") Double latitude) {
-        LOG.info("REST request health check");
+    TimeZoneEngine engine;
+
+    @PostConstruct
+    private void init() {
+        LOG.info("Initializing Service >>> " );
+
+        LOG.info(" ... Loading Timezone Database " );
+        engine = TimeZoneEngine.initialize();
+
+        LOG.info("<<< Initializing Service " );
+
+    }
+
+    @GetMapping(value = "/timezones", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GeolocInfo> getLocation(
+            @RequestParam(name = "longitude") Double longitude,
+            @RequestParam(name = "latitude") Double latitude
+    ) {
+        LOG.info("Get timezone from position called longitude = " + longitude + " latitude = " + latitude);
         GeolocInfo geolocInfo = new GeolocInfo();
-        geolocInfo.setLongitude( longitude);
+        geolocInfo.setLongitude(longitude);
         geolocInfo.setLatitude(latitude);
-        geolocInfo.setTimezoneId("abc");
+
+        Optional<ZoneId> maybeZneId = engine.query(latitude, longitude);
+
+        if (!maybeZneId.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        geolocInfo.setTimezoneId(maybeZneId.get().toString());
         return new ResponseEntity<>(geolocInfo, HttpStatus.OK);
     }
 
